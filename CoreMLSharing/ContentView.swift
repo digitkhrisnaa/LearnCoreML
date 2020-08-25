b52d27d409c724758f99655d7522d3d840a32ff1
Digital Khrisna <digital.aurum@tokopedia.com>
2020-08-25 10:51:30 +0700

    Initial Commit

---
+++ CoreMLSharing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreMLSharing
+//
+//  Created by Aurum on 25/08/20.
+//  Copyright © 2020 Digital Khrisna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
